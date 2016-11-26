c9c3a4445e05658081285ee34b34b8c5259b0a2b
nplagman <nplagman@dunwoodylabs.com>
2016-11-25 23:36:47 -0500

    figured out input field

--- script.js
+++ script.js
@@ -9,11 +9,38 @@ $(".well").hover(function() {
 });
 
 
-$(".well").click(function() {
+//would prefer to not have to do one of these for every question; there must be a better way.
+$("#A1").click(function() {
  $(".question").show();
-});
-             
+ $('<h4>The \"MVP\" quarterback whose team is 14-6 when he doesn’t play.</h4>').appendTo('.question');
+ $('#submit').click(function() {
+ 	
+ }
 
+
+//would prefer instead of writing question in those p tags, say the ID and go grab it from JSON
+//I have to change the p to an h4 or something; better way?
+
+
+
+//   var answer = prompt("Which river flows through Washington, D.C.?");
+//     if (answer === "Potomac River"){
+//       var questionValue = 200;
+//       currentTotal+= questionValue;
+//       $("#currentTotal").text("$" + currentTotal);
+//       alert("Correct!");
+//     }
+//     else {
+//       var questionValue = 200;
+//       currentTotal -= questionValue;
+//       $("#currentTotal").text("$" + currentTotal);
+//       alert("Sorry, " + name + ", that's not correct.");
+//     }
+//    $("#one200").text("");
+// })
+             
+});
+});
 
 
 
@@ -31,5 +58,3 @@ $(".well").click(function() {
 // $('div').hover(function(){
 //   $(this).addClass('green');
 // });
-
-});
